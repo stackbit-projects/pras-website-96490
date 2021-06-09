123dc34c3cfaee4066c7e6c057f2934c2c2739f7
Stackbit Code Editor <projects@stackbit.com>
2021-06-09 00:11:51 +0000

    SectionBackground.js: changed

--- src/components/SectionBackground.js
+++ src/components/SectionBackground.js
@@ -17,6 +17,7 @@ export default class SectionBackground extends React.Component {
                 className="cover-img"
                 style={{
                     backgroundImage: `url('${withPrefix(backgroundImage)}')`,
+                    backgroundColor: blue,
                     opacity: backgroundImageOpacity,
                     backgroundSize: backgroundImageSize,
                     backgroundPosition: backgroundImagePosition,
